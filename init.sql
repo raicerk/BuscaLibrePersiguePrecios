b8624f53ea81576f43e9c9c7db8a9b11cfcc4622
Juan Mora <raicerk@gmail.com>
2019-11-18 11:16:33 -0300

    Revert "Se agrega nuevo campo a db para estado de link para un usuario"
    This reverts commit 958c33cabb2db6f1d6b152c8ec0c49b10259ffe8.

--- init.sql
+++ init.sql
@@ -6,9 +6,9 @@
 CREATE TABLE public.link (
 	id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
 	link varchar NULL DEFAULT true,
+	estado bool NULL,
 	nombre varchar NULL,
-	autor varchar NULL,
-	estado bool NULL
+	autor varchar NULL
 );
 CREATE UNIQUE INDEX link_link_idx ON public.link (link);
 CREATE UNIQUE INDEX links_id_idx ON public.link (id);
@@ -34,10 +34,9 @@ CREATE UNIQUE INDEX precios_id_idx ON public.precio USING btree (id);
 -- DROP TABLE public.usuario_link;
 
 CREATE TABLE public.usuario_link (
-	id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
 	idusuario int4 NULL,
 	idlink int4 NULL,
-	estado bool NULL
+	id int4 NOT NULL GENERATED ALWAYS AS IDENTITY
 );
 CREATE UNIQUE INDEX usuario_link_idlink_idx ON public.usuario_link (idlink,idusuario);
 CREATE UNIQUE INDEX usuario_link_id_idx ON public.usuario_link (id);
@@ -52,8 +51,8 @@ CREATE UNIQUE INDEX usuario_link_id_idx ON public.usuario_link (id);
 CREATE TABLE public.usuariotelegram (
 	id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
 	idusuario int4 NOT NULL,
-	idchat int4 NULL,
-	estado bool NULL
+	estado bool NULL,
+	idchat int4 NULL
 );
 CREATE UNIQUE INDEX usuariotelegram_idchat_idx ON public.usuariotelegram (idchat,idusuario);
 CREATE UNIQUE INDEX usuariotelegram_id_idx ON public.usuariotelegram (id);
