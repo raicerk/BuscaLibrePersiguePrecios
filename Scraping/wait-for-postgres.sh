8b63db0e7b4c116cc9ac4f59f84ba06146f1ee52
Juan Mora <raicerk@gmail.com>
2019-11-19 23:54:44 -0300

    Se agrega datos de variables de entorno

--- Scraping/wait-for-postgres.sh
+++ Scraping/wait-for-postgres.sh
@@ -7,7 +7,7 @@ host="$1"
 shift
 cmd="$@"
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U $POSTGRES_USER -c '\q'; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
